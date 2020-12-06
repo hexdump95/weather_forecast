4d772ae329d8165307f085663e401bd6c2ec9ed9
Sergio Villanueva <sergionicolasvillanueva@gmail.com>
2020-12-06 18:06:01 -0300

    Set portrait mode only

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,21 @@
-import 'package:intl/date_symbol_data_local.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/date_symbol_data_local.dart';
 
 import './common/theme.dart';
 import './screens/error_page.dart';
 import './screens/home.dart';
 import './screens/today_forecast.dart';
 
-void main() =>
-    initializeDateFormatting('es_AR', null).then((_) => runApp(MyApp()));
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setEnabledSystemUIOverlays([]);
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitDown,
+    DeviceOrientation.portraitUp,
+  ]);
+  initializeDateFormatting('es_AR', null).then((_) => runApp(MyApp()));
+}
 
 class MyApp extends StatelessWidget {
   @override
