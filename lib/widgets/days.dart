ba06577882993ec82825ed047cbf60779d0a0d6d
Sergio Villanueva <sergionicolasvillanueva@gmail.com>
2020-12-06 17:15:01 -0300

    Show temperature of the following days

--- lib/widgets/days.dart
+++ lib/widgets/days.dart
@@ -1,7 +1,39 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 import 'package:intl/intl.dart';
 
-class Days extends StatelessWidget {
+import '../models/temp_day.dart';
+import '../models/temp_day_list.dart';
+
+class Days extends StatefulWidget {
+  @override
+  _DaysState createState() => _DaysState();
+}
+
+class _DaysState extends State<Days> {
+  var _myUri2 = '';
+
+  Future<List<TempDay>> fetchWeather() async {
+    final response = await http.get(_myUri2);
+    if (response.statusCode == 200) {
+      var weatherArray =
+          TempDayList.fromJson(jsonDecode(response.body)).weathers;
+      return weatherArray;
+    } else {
+      throw Exception('Error!');
+    }
+  }
+
+  Future<List<TempDay>> futureWeatherList;
+
+  @override
+  void initState() {
+    super.initState();
+    futureWeatherList = fetchWeather();
+  }
+
   String _currentDay(int index) {
     return DateFormat.E('es_AR')
         .format(DateTime.now().add(Duration(days: index)))
@@ -14,43 +46,55 @@ class Days extends StatelessWidget {
     return FittedBox(
       fit: BoxFit.scaleDown,
       child: Container(
-          padding: const EdgeInsets.all(10),
-          height: 350,
-          width: 300,
-          child: Container(
-            height: 200,
-            child: ListView.builder(
-              itemBuilder: (BuildContext context, int index) {
-                return Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Container(
-                      width: 30,
-                      child: FittedBox(
-                        child: Text(
-                          _currentDay(index),
-                          style: const TextStyle(
-                            color: Colors.white,
+        padding: const EdgeInsets.all(10),
+        height: 350,
+        width: 300,
+        child: FutureBuilder<List<TempDay>>(
+            future: futureWeatherList,
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                return Container(
+                  height: 200,
+                  child: ListView.builder(
+                    itemBuilder: (BuildContext context, int index) {
+                      return Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Container(
+                            width: 30,
+                            child: FittedBox(
+                              child: Text(
+                                _currentDay(index),
+                                style: const TextStyle(
+                                  color: Colors.white,
+                                ),
+                              ),
+                            ),
                           ),
-                        ),
-                      ),
-                    ),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        const SizedBox(height: 12),
-                        Text('25°C MAX'),
-                        const SizedBox(height: 5),
-                        Text('10°C MIN'),
-                        const SizedBox(height: 12),
-                      ],
-                    ),
-                  ],
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              const SizedBox(height: 12),
+                              Text(
+                                  '${snapshot.data[index].tempMax.toString()} °C MAX'),
+                              const SizedBox(height: 5),
+                              Text(
+                                  '${snapshot.data[index].tempMin.toString()} °C MIN'),
+                              const SizedBox(height: 12),
+                            ],
+                          ),
+                        ],
+                      );
+                    },
+                    itemCount: snapshot.data.length,
+                  ),
                 );
-              },
-              itemCount: 6,
-            ),
-          )),
+              } else if (snapshot.hasError) {
+                return Text("${snapshot.error}");
+              }
+              return Container();
+            }),
+      ),
     );
   }
 }
